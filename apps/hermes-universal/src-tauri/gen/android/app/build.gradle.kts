898de34cce17061bdb3b972942a80135e685010b
Jai Shukla <jaishukla7768@gmail.com>
2026-08-03 20:08:20 +0530

    chore(universal): rename bundle id to com.jaxmatrix.mjx-unofficial-hermes

--- apps/hermes-universal/src-tauri/gen/android/app/build.gradle.kts
+++ apps/hermes-universal/src-tauri/gen/android/app/build.gradle.kts
@@ -15,10 +15,10 @@ val tauriProperties = Properties().apply {
 
 android {
     compileSdk = 36
-    namespace = "com.nousresearch.hermes.universal"
+    namespace = "com.jaxmatrix.mjx_unofficial_hermes"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.nousresearch.hermes.universal"
+        applicationId = "com.jaxmatrix.mjx_unofficial_hermes"
         minSdk = 24
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
